17ed84d8127ae00b2fa64bf86a7e95dfdc008656
AshkanAbd <kotksthegame@gmail.com>
2018-11-15 23:23:41 +0330

    ashkan

--- java/src/Main.java
+++ java/src/Main.java
@@ -1,13 +1,10 @@
-import LinkedList.*;
+import SortAlgorithms.SortAlgorithms;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        DoublyLinkedList<Integer> linkedList = new DoublyLinkedList<Integer>();
-        linkedList.add(1);
-        linkedList.add(2);
-        linkedList.add(3);
-        linkedList.add(4);
-        linkedList.remove(0);
-        System.out.println(linkedList.toList());
+        int a[] = {2, 5, 1, 8, 4, 3, 9, 6, 7};
+        System.out.println(Arrays.toString(SortAlgorithms.bubbleSort(a)));
     }
 }
--- java/src/SortAlgorithms/SortAlgorithms.java
+++ java/src/SortAlgorithms/SortAlgorithms.java
@@ -1,4 +1,27 @@
 package SortAlgorithms;
 
+import java.util.Arrays;
+
 public class SortAlgorithms {
+
+    public static int[] bubbleSort(int[] array) {
+        int[] dst = new int[array.length];
+        int size = array.length;
+        System.arraycopy(array, 0, dst, 0, array.length);
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (dst[j] > dst[j + 1])
+                    swap(dst, j, j + 1);
+
+            }
+        }
+        return dst;
+    }
+
+    private static void swap(int[] arr, int a, int b) {
+        int c = arr[a];
+        arr[a] = arr[b];
+        arr[b] = c;
+    }
+
 }
